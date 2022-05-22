a1f3e245bc401c17e78c2af8ff55270c81ccb6d4
Bishal Dali <bishalsthaktm12@gmail.com>
2022-05-22 21:57:12 +0545

    Register component created

--- frontend/src/screens/Register/index.jsx
+++ frontend/src/screens/Register/index.jsx
@@ -13,20 +13,14 @@ import FormContainer from '../../components/FormContainer'
 
 const Register = ({ location, history }) => {
 
-    const [name, setName] = useState('')
+    const [firstName, setFirstName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [cropSelection, setCropSelection] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
-    const [fatherName, setFatherName] = useState('')
-    const [motherName, setMotherName] = useState('')
-    const [spouseName, setSpouseName] = useState('')
-    const [country, setCountry] = useState('Nepal')
-    const [province, setProvince] = useState('Province 1')
-    const [address, setAddress] = useState('')
-    const [gender, setGender] = useState('Male')
-    const [maritalStatus, setMaritalStatus] = useState('Single')
-    const [message, setMessage] = useState(null)
+    const [lastName, setLastName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [gender, setGender] = useState('')
+   
 
     // const dispatch = useDispatch()
 
@@ -50,6 +44,7 @@ const Register = ({ location, history }) => {
     //         dispatch(register(name, email, password, cropSelection, fatherName, motherName, spouseName, gender, maritalStatus, country, address, province))
     //     }
     // }
+    }
 
     return (
 
@@ -59,32 +54,98 @@ const Register = ({ location, history }) => {
             {error && <Message variant='danger'>{error}</Message>}
             {loading && <Loader />} */}
             <Form onSubmit={submitHandler}>
-                <Row>
-                    <Col md={6}>
-                        <Form.Group controlId='name'>
-                            <Form.Label>Name <span style={{ color: 'red' }}>*</span></Form.Label>
+                        <Form.Group controlId='firstName'>
+                            <Form.Label style={{ marginTop: '10px'}}>First Name <span style={{ color: 'red' }}>*</span></Form.Label>
                             <Form.Control
                                 type="name"
-                                placeholder="Enter name"
-                                value={name}
+                                placeholder="Enter first name"
+                                value={firstName}
                                 required
-                                onChange={(e) => setName(e.target.value)}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId='lastName'>
+                            <Form.Label style={{ marginTop: '10px'}}>Last Name<span style={{ color: 'red' }}>*</span></Form.Label>
+                            <Form.Control
+                                type="lastName"
+                                placeholder="Enter last name"
+                                value={lastName}
+                                required
+                                onChange={(e) => setLastName(e.target.value)}
                             ></Form.Control>
                         </Form.Group>
                         <Form.Group controlId='email'>
-                            <Form.Label>Email Address<span style={{ color: 'red' }}>*</span></Form.Label>
+                            <Form.Label style={{ marginTop: '10px'}}>Email Address<span style={{ color: 'red' }}>*</span></Form.Label>
                             <Form.Control
-                                type="nic"
-                                placeholder="Enter email"
+                                type="email"
+                                placeholder="Enter email address"
                                 value={email}
                                 required
                                 onChange={(e) => setEmail(e.target.value)}
                             ></Form.Control>
                         </Form.Group>
+                        <Form.Group controlId='phoneNumber'>
+                            <Form.Label style={{ marginTop: '10px'}}>Phone Number<span style={{ color: 'red' }}>*</span></Form.Label>
+                            <Form.Control
+                                type="phone"
+                                placeholder="Enter phone number"
+                                value={phone}
+                                required
+                                onChange={(e) => setPhone(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId='password'>
+                            <Form.Label style={{ marginTop: '10px'}}>Password<span style={{ color: 'red' }}>*</span></Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Enter new password"
+                                value={password}
+                                required
+                                onChange={(e) => setPassword(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId='confirmPassword'>
+                            <Form.Label style={{ marginTop: '10px'}}>Confirm Password<span style={{ color: 'red' }}>*</span></Form.Label>
+                            <Form.Control
+                                type="confirmPassword"
+                                placeholder="Enter your password again"
+                                value={confirmPassword}
+                                required
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                        <Form.Label style={{ marginTop: '10px'}}>Select Gender</Form.Label>
+
+                        <div className='container  p-1'>
+                <select Classname="custom select" 
+                 required
+                 style={{fontSize: 17, color: 'grey'}}
+                onChange={(e) => {
+                    const selectedGender = e.target.value;
+                    setGender(selectedGender)
+                }}
+                >
+                    <option value="Male" >Male</option>
+                    
+                    <option value="Female" > Female </option>
+
+                    <option value="Bisexual" > Bisexual </option>
+
+                    <option value="Homosexual" > Homosexual </option>
+
+                    <option value="Bitch" > Bitch </option>
+
+                    <option value="Not preffered to say" > Not preffered to say </option>
+
+                    </select>
+                    
+                    </div>
+
+                    </Form.Group>
                      
-                        <Button type="submit" variant="primary">Register</Button>
-                    </Col>
-                </Row>
+                        <Button type="submit" variant="primary" style={{backgroundColor: "black", marginTop: '10px'}}>Register</Button>
+                
             </Form>
             
             <Row className='py-3'>
@@ -95,6 +156,6 @@ const Register = ({ location, history }) => {
         </FormContainer>
     )
 }
-}
+
 
 export default Register
